8794cd52e5796851290225ed96f606bbd3b706e2
Abdul-Rahman Mirza <abdul_mirza805@hotmail.com>
2022-06-08 13:06:30 -0400

    Changed code to labcode

--- cifarloader.py
+++ cifarloader.py
@@ -32,12 +32,12 @@ class CNN(nn.Module):
         )
         self.fc_layer = nn.Sequential(
             nn.Dropout(p=0.1),
-            nn.Linear(512, 512),
+            nn.Linear(8 * 8 * 64, 1000),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 512),
+            nn.Linear(1000, 512),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.1),
-            nn.Linear(512, 512)
+            nn.Linear(512, 10)
         )
 
     def forward(self, x):
@@ -56,13 +56,14 @@ num_classes = 4
 learning_rate = 0.001
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-batch_size = 32
+batch_size = 8
 
 transform = transforms.Compose(
 		[transforms.ToTensor(),
-		 transforms.Resize((512,512)),
+		 transforms.Resize((4096, 4096)),
 		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
 	)
+	
 root = 'dataset'
 # tuple of (path, label)
 dirsTraining = [
@@ -87,90 +88,88 @@ def collate_fn(batch):
    batch = list(filter(lambda x: x is not None, batch))
    return torch.utils.data.dataloader.default_collate(batch) 
 
-dataLoaderTrain = DataLoader(dataset=datasetTraining, batch_size =batch_size, shuffle=True, collate_fn=collate_fn)
-dataLoaderTest = DataLoader(dataset=datasetTest, batch_size =batch_size, shuffle=True, collate_fn=collate_fn)
+# dataLoaderTrain = DataLoader(dataset=datasetTraining, batch_size =batch_size, shuffle=True, collate_fn=collate_fn)
+# dataLoaderTest = DataLoader(dataset=datasetTest, batch_size =batch_size, shuffle=True, collate_fn=collate_fn)
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 print("Begin training", DEVICE)
 
-#y_train = np.array([y for x, y in iter(train_data)])
+y_train = np.array([y for x, y in iter(datasetTraining)])
 
 classes = ('no mask', 'surgical mask', 'cloth mask',  'n95')
 
 torch.manual_seed(0)
-# net = NeuralNetClassifier(
-# 		CNN,
-# 		max_epochs=1,
-# 		iterator_train__num_workers=0,
-# 		iterator_valid__num_workers=0,
-# 		lr=1e-3,
-# 		batch_size=64,
-# 		optimizer=optim.Adam,
-# 		criterion=nn.CrossEntropyLoss,
-# 		device=DEVICE
-# 	)
+net = NeuralNetClassifier(
+		CNN,
+		max_epochs=1,
+		iterator_train__num_workers=0,
+		iterator_valid__num_workers=0,
+		lr=1e-3,
+		batch_size= batch_size,#64,
+		optimizer=optim.Adam,
+		criterion=nn.CrossEntropyLoss,
+		device=DEVICE
+	)
+
+net.fit(datasetTraining, y=y_train)
+
+print("Done fitting data")
+
+
+y_pred = net.predict(datasetTest)
+y_test = np.array([y for x, y in iter(datasetTest)])
+accuracy_score(y_test, y_pred)
+plot_confusion_matrix(net, datasetTest, y_test.reshape(-1, 1))
+plt.show()
+
+print("115")
+
+net.fit(datasetTraining, y=y_train)
+train_sliceable = SliceDataset(datasetTraining)
+scores = cross_val_score(net, train_sliceable, y_train, cv=5,
+scoring="accuracy")
+
+print("program done")
+# for epoch in range(num_epochs):
+#    losses = []
+
+#    for batch_idx, (data, targets) in enumerate(dataLoaderTrain):
+#      data = data.to(device=DEVICE)
+#      targets = targets.to(device=DEVICE)
+
+#      scores = model(data)
+#      loss = criterion(scores, targets) 
+
+#      losses.append(loss.item())
+
+#      optimizer.zero_grad()
+#      loss.backward()
+
+#      optimizer.step()
+
+
+# # Check accuracy on training to see how good our model is
+# def check_accuracy(loader, model):
+# 	num_correct = 0
+# 	num_samples = 0
+# 	model.evalO
+# 	with torch.no_grad():
+# 		for x, y in loader:
+# 			x = x.to(device=device)
+# 			y = y.to(device=device)
+# 			scores = model(x)
+# 			_, predictions = scores.max(1)
+# 			num_correct += (predictions == y).sum()
+# 			num_samples += predictions.size(e)
+# 		# print(f'Got {num_correct} / {num_samples} with accuracy {float (num_correct)/float(num_samples)*1})
+# 	model.train()
 
 
 
-# net.fit(train_data, y=y_train)
+# print ('Checking accuracy on Training Set')
+# check_accuracy(train_loader, model)
 
-# print("Done fitting data")
-
-
-# y_pred = net.predict(testset)
-# y_test = np.array([y for x, y in iter(testset)])
-# accuracy_score(y_test, y_pred)
-# plot_confusion_matrix(net, testset, y_test.reshape(-1, 1))
-# plt.show()
-
-# print("115")
-
-# net.fit(train_data, y=y_train)
-# train_sliceable = SliceDataset(train_data)
-# scores = cross_val_score(net, train_sliceable, y_train, cv=5,
-# scoring="accuracy")
-
-# print("program done")
-for epoch in range(num_epochs):
-   losses = []
-
-   for batch_idx, (data, targets) in enumerate(dataLoaderTrain):
-     data = data.to(device=DEVICE)
-     targets = targets.to(device=DEVICE)
-
-     scores = model(data)
-     loss = criterion(scores, targets) 
-
-     losses.append(loss.item())
-
-     optimizer.zero_grad()
-     loss.backward()
-
-     optimizer.step()
-
-
-# Check accuracy on training to see how good our model is
-def check_accuracy(loader, model):
-	num_correct = 0
-	num_samples = 0
-	model.evalO
-	with torch.no_grad():
-		for x, y in loader:
-			x = x.to(device=device)
-			y = y.to(device=device)
-			scores = model(x)
-			_, predictions = scores.max(1)
-			num_correct += (predictions == y).sum()
-			num_samples += predictions.size(e)
-		# print(f'Got {num_correct} / {num_samples} with accuracy {float (num_correct)/float(num_samples)*1})
-	model.train()
-
-
-
-print ('Checking accuracy on Training Set')
-check_accuracy(train_loader, model)
-
-print ('Checking accuracy on Test Set')
-check_accuracy(test_loader, model)
+# print ('Checking accuracy on Test Set')
+# check_accuracy(test_loader, model)
 
