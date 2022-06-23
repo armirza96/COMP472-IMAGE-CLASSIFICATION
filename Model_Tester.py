c0d199a41514b05509a36136b75a79109a0a635e
Abdul-Rahman Mirza <abdul_mirza805@hotmail.com>
2022-06-23 00:20:49 -0400

    Commit for done

--- Model_Tester.py
+++ Model_Tester.py
@@ -87,8 +87,8 @@ transform = transforms.Compose(
 
 
 # 2) Change Root by uncommenting the line you wish to test
-root = 'Dataset_General'
-#root = 'Dataset_Test_Bias_Gender'
+#root = 'Dataset_General'
+root = 'Dataset_Test_Bias_Gender'
 #root = 'Dataset_Test_Bias_Race'
 
 
@@ -110,43 +110,83 @@ for dir in dirs:
 
 # 3) Once again, uncomment the lines you wish to test
 
-test_NoMask_General = items[0][299:399]
-#test_NoMask_Bias_Gender = items[0][0:10]
+#test_NoMask_General = items[0][299:399]
+test_NoMask_Bias_Gender = items[0]
 #test_NoMask_Bias_Race = items[0][0:10]
 
-test_SurgicalMask_General = items[1][299:399] 
-#test_SurgicalMask_Bias_Gender = items[1][0:10]
+#test_SurgicalMask_General = items[1][299:399] 
+test_SurgicalMask_Bias_Gender = items[1]
 #test_SurgicalMask_Bias_Race = items[1][0:10]
 
-test_ClothMask_General = items[2][299:399]
-#test_ClothMask_Bias_Gender = items[2][0:10]
+#test_ClothMask_General = items[2][299:399]
+test_ClothMask_Bias_Gender = items[2]
 #test_ClothMask_Bias_Race = items[2][0:10]
 
-test_N95_General = items[3][299:399]
-#test_N95_Bias_Gender = items[3][0:10]
+#test_N95_General = items[3][299:399]
+test_N95_Bias_Gender = items[3]
 #test_N95_Bias_Race = items[3][0:10]
 
 
 # 4) Uncomment the lines you wish to test
 
-testing_General = test_NoMask_General + test_SurgicalMask_General + test_ClothMask_General + test_N95_General
-#testing_Bias_Gender = test_NoMask_Bias_Gender + test_SurgicalMask_Bias_Gender + test_ClothMask_Bias_Gender + test_N95_Bias_Gender 
+#testing_General = test_NoMask_General + test_SurgicalMask_General + test_ClothMask_General + test_N95_General
+testing_Bias_Gender = test_NoMask_Bias_Gender + test_SurgicalMask_Bias_Gender + test_ClothMask_Bias_Gender + test_N95_Bias_Gender 
 #testing_Bias_Race = test_NoMask_Bias_Race + test_SurgicalMask_Bias_Race + test_ClothMask_Bias_Race + test_N95_Bias_Race 
 
-print("testing")
+#print("testing", testing_General[50], testing_General[150], testing_General[250],testing_General[350])
 
 
 # 5) In the below line, replace 'testing_General' with the dataset you wish to test, if you changed it
-datasetTest = MasksDataSet(testing_General, transform = transform)
+datasetTest = MasksDataSet(testing_Bias_Gender, transform = transform)
 
-y_pred = net.predict(datasetTest)
-y_test = np.array([y for x, y in iter(datasetTest)])
-plot_confusion_matrix(net, datasetTest, y_test.reshape(-1, 1))
+# y_pred = net.predict(datasetTest)
+# y_test = np.array([y for x, y in iter(datasetTest)])
+# plot_confusion_matrix(net, datasetTest, y_test.reshape(-1, 1))
 
-print("Accuracy:    ", accuracy_score(y_test, y_pred)*100)
-print("Recall:      ", recall_score(y_true=y_test, y_pred=y_pred, average='weighted')*100)
-print("Precision:   ", precision_score(y_true=y_test, y_pred=y_pred, average='weighted')*100)
-print("F1_Score:    ", f1_score(y_true=y_test, y_pred=y_pred, average='weighted')*100)
+bar_graph_values = [[],[],[],[]]
 
-print("program done")
+labels = ['No Mask', 'Surgical', 'Cloth', 'N95']
+true_positives = [0, 0, 0, 0]
+false_positives = [0, 0, 0, 0]
+  
+plt.title(f'True positives vs False positives')
+
+for img in datasetTest:
+    portion = img # tuple of (image, label)
+    image = portion[0] # image that we are working with from the dataset test index
+    true_target = portion[1] # label of the image
+
+    # Reshape image
+    #image = image.reshape(64, 64, 1)
+
+    #x = transform(image)  # Preprocess image
+    x = image.unsqueeze(0)  # Add batch dimension
+
+    # Generate prediction
+    prediction = model(x)
+
+    # Predicted class value using argmax
+    predicted_class = torch.argmax(prediction)
+
+    # Show result
+    #plt.imshow(image)
+    #plt.title(f'Prediction: {predicted_class} - Actual target: {true_target}')
+    #plt.show()
+    #print(f'Prediction: {predicted_class} - Actual target: {true_target}')
+
+    if predicted_class == true_target:
+        true_positives[true_target] = true_positives[true_target] + 1
+    else:
+        false_positives[true_target] = false_positives[true_target] + 1
+    # print("Accuracy:    ", accuracy_score(y_test, y_pred)*100)
+    # print("Recall:      ", recall_score(y_true=y_test, y_pred=y_pred, average='weighted')*100)
+    # print("Precision:   ", precision_score(y_true=y_test, y_pred=y_pred, average='weighted')*100)
+    # print("F1_Score:    ", f1_score(y_true=y_test, y_pred=y_pred, average='weighted')*100)
+
+    # print("program done")
+    # plt.show()
+print(true_positives, false_positives)
+# plot bars in stack manner
+plt.bar(labels, true_positives, color='g')
+plt.bar(labels, false_positives, bottom=true_positives, color='r')
 plt.show()
